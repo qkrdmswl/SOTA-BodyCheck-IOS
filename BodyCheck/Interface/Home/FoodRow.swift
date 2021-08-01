0873a51c5460ee27a4ab329b120aaaa4b03cf52a
HyunSBong <sanseng@mju.ac.kr>
2021-08-01 11:12:00 +0900

    Home

---
+++ BodyCheck/Interface/Home/FoodRow.swift
@@ -0,0 +1,75 @@
+//
+//  FoodRow.swift
+//  BodyCheck
+//
+//  Created by hyunsubong on 2021/07/31.
+//
+
+import SwiftUI
+
+struct FoodRow: View {
+    let food: Food
+    
+    var body: some View {
+        HStack {
+            foodImage
+            foodDescription
+        }
+        .frame(height: 150)
+        .background(Color.primary.colorInvert())
+    }
+}
+
+private extension FoodRow {
+    
+    var foodImage: some View {
+        Image(food.imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+
+    var foodDescription: some View {
+        VStack(alignment: .leading) {
+            Text(food.name)
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 1)
+
+            Text(food.description)
+                .font(.footnote)
+                .foregroundColor(.secondary)
+
+            Spacer()
+
+            footerView
+        }
+        .padding([.leading, .bottom], 10)
+        .padding([.top, .trailing])
+    }
+
+    var footerView: some View {
+        HStack(spacing: 0) {
+            Text("500").font(.headline)
+                + Text(" kcal").font(.footnote)
+
+            Spacer()
+
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(.secondary)
+                .frame(width: 30, height: 30)
+
+            Image(systemName: "plus")
+                .foregroundColor(.secondary)
+                .frame(width: 30, height: 30)
+        }
+    }
+}
+
+struct FoodRow_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodRow(food: foodSamples[0])
+    }
+}
